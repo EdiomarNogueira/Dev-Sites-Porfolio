8eadf6e8e6812ddd0cd1205b3584b89b1a238ba1
Ediomar.Nogueira <ediomar_ti@hotmail.com>
2020-10-25 03:49:07 -0300

    Atualização para tornar mais generico

--- ProvedorInternet/ProvedorInternet/JS/slide.js
+++ ProvedorInternet/ProvedorInternet/JS/slide.js
@@ -1,24 +1,37 @@
 var x = document.getElementById('banner1');
 var y = document.getElementById('banner2');
+var z = document.getElementById('banner3');
 
 function anterior () {
         
-    if (x.style.display ==='none') {
+    if ((x.style.display ==='none') && (y.style.display ==='none')) {
         x.style.display ='flex';
         y.style.display ='none';
-    } else if (y.style.display ='none'){
+        z.style.display ='none';
+    } else if ((x.style.display ==='none') && (y.style.display ==='flex')){
+        x.style.display ='none';
+        y.style.display ='none';
+        z.style.display ='flex';
+    }else {
         x.style.display ='none';
         y.style.display ='flex';
+        z.style.display ='none';
     };
 };
 function proximo () {
        
-    if (x.style.display ==='none') {      
+    if ((x.style.display ==='none') && (y.style.display ==='none')) {
         x.style.display ='flex';
         y.style.display ='none';
-    } else {
+        z.style.display ='none';
+    } else if ((x.style.display ==='none') && (y.style.display ==='flex')){
+        x.style.display ='none';
+        y.style.display ='none';
+        z.style.display ='flex';
+    }else {
         x.style.display ='none';
         y.style.display ='flex';
+        z.style.display ='none';
     };
 };
 
